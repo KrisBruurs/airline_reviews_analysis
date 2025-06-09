77ccbc2bd4942b19e43f458f39dca0f8e0b4b09f
Kris Bruurs <krisbruurs@gmail.com>
2025-06-09 22:01:26 +0200

    started topic modeling

--- 03_topic_model.R
+++ 03_topic_model.R
@@ -4,3 +4,12 @@ library(tidyverse)
 ###---Input---###
 pos_rev <- read_csv('data/positive_reviews.csv')
 neg_rev <- read_csv('data/negative_reviews.csv')
+
+###---Transform---###
+pos_rev %>% 
+  group_by(`Airline Name`) %>% 
+  count(sort = TRUE)
+
+neg_rev %>% 
+  group_by(`Airline Name`) %>% 
+  count(sort = TRUE)
